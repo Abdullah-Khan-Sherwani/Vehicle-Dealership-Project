caae74349e7c6dc816c8d1674665ea2857431bcd
Muhammad Hamza Kashif <68157276+hamzahisam@users.noreply.github.com>
2024-03-15 18:09:09 +0500

    header_file(v2).cpp
    Added new display functions

--- Vehicle Dealership Project/header_file.cpp
+++ Vehicle Dealership Project/header_file.cpp
@@ -38,9 +38,9 @@ class cars : public vehicles{
         }
     }
 
-    void add_car(string make, string model, string variant, int year, string number_plate, string engine, string mileage, double price){
+    void add_car(string make, string model, string variant, int year, string number_plate, string engine, string mileage, int price){
         number_of_cars += 1;
-        string new_car = make + " " + model + " " + variant + " " + to_string(year) + " " + number_plate + " " + engine + " " + mileage;
+        string new_car = make + " " + model + " " + variant + " " + to_string(year) + " " + number_plate + " " + engine + " " + mileage + " " + to_string(price);
         car_list.push_back(new_car);
     }
 
@@ -79,14 +79,16 @@ class cars : public vehicles{
     void display_car(string number_plate){
         int index = search_car(number_plate);
         if(index != -1){
-            cout<< "Make|Model|Variant|Year|Number-Plate|Engine|Mileage|Price\n\n";
+            cout<<"\n-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-";
+            cout<< "\nMake|Model|Variant|Year|Number-Plate|Engine|Mileage|Price\n\n";
             cout<< car_list[index] << endl;
         }
     }
 
     void display_by_make(string make){
         bool found = false;
-        cout<<"Make|Model|Variant|Year|Number-Plate|Engine|Mileage|Price\n\n";
+        cout<<"\n-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-";
+        cout<<"\nMake|Model|Variant|Year|Number-Plate|Engine|Mileage|Price\n\n";
 
         for(int i = 0; i < number_of_cars; i++){
             string details = car_list[i];
@@ -109,7 +111,97 @@ class cars : public vehicles{
             }
         }
         if(!found){
-            cout<<"There is no car by that make in our inventory\n";
+            cout<<"\nThere is no car by that make in our inventory\n";
+        }
+    }
+
+    void display_by_model(string model){
+        bool found = false;
+        cout<<"\n-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-";
+        cout<<"\nMake|Model|Variant|Year|Number-Plate|Engine|Mileage|Price\n\n";
+
+        for(int i = 0; i < number_of_cars; i++){
+            string details = car_list[i];
+            int column = 0;
+            string name = "";
+
+            for(int j = 0; j < details.length(); j++){
+                string index_char = details.substr(j, 1);
+                if(index_char != " " && column == 1){
+                    name += index_char;
+                }
+                if(index_char == " "){
+                    column += 1;
+                }
+            }
+            
+            if(name == model){
+                found = true;
+                cout<< car_list[i] << endl;
+            }
+        }
+        if(!found){
+            cout<<"\nThere is no car with that model name in our inventory";
+        }
+    }
+
+    void display_by_year(int year){
+        bool found = false;
+        cout<<"\n-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-";
+        cout<<"\nMake|Model|Variant|Year|Number-Plate|Engine|Mileage|Price\n\n";
+
+        for(int i = 0; i < number_of_cars; i++){
+            string details = car_list[i];
+            int column = 0;
+            string y = "";
+
+            for(int j = 0; j < details.length(); j++){
+                string index_char = details.substr(j, 1);
+                if(index_char != " " && column == 3){
+                    y += index_char;
+                }
+                if(index_char == " "){
+                    column += 1;
+                }
+            }
+            
+            if(y != "" && stoi(y) == year){
+                found = true;
+                cout<< car_list[i] << endl;
+            }
+        }
+        if(!found){
+            cout<<"\nThere is no car with that model name in our inventory";
+        }
+    }
+
+    void display_within_price_range(int min, int max){
+        bool found = false;
+        cout<<"\n-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-";
+        cout<<"\nMake|Model|Variant|Year|Number-Plate|Engine|Mileage|Price\n\n";
+
+        for(int i = 0; i < number_of_cars; i++){
+            string details = car_list[i];
+            int column = 0;
+            string value = "";
+
+            for(int j = 0; j < details.length(); j++){
+                string index_char = details.substr(j, 1);
+                if(index_char != " " && column == 7){
+                    value += index_char;
+                }
+                if(index_char == " "){
+                    column += 1;
+                }
+            }
+            
+            if(value != "" && stoi(value) <= max && stoi(value) >= min){
+                found = true;
+                cout<< car_list[i] << endl;
+            }
+        }
+        if(!found){
+            cout<<"\nThere is no car within that price range in our inventory";
         }
     }
 
@@ -122,7 +214,8 @@ class cars : public vehicles{
     }
 
     void display_all_cars(){
-        cout<< "Make|Model|Variant|Year|Number-Plate|Engine|Mileage|Price\n\n";
+        cout<<"\n-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-";
+        cout<< "\nMake|Model|Variant|Year|Number-Plate|Engine|Mileage|Price\n\n";
         for(int i = 0; i < number_of_cars; i++){
             cout<<car_list[i] << endl << endl;
         }
@@ -141,9 +234,17 @@ int main(){
     a.add_car("Lamborghini", "Aventador", "SVJ", 2022, "X-88-X", "6500cc", "5km/l", 400000000);
 
     //a.display_car("BYX-365");
-    a.remove_car("BYX-365");
+    //a.remove_car("BYX-365");
 
     //a.display_all_cars();
 
-    a.display_by_make("Honda");
+    //a.display_by_make("Honda");
+
+    //a.display_within_price_range(4500000, 8000000);
+
+    //a.display_car("X-88-X");
+
+    //a.display_by_model("Alto");
+
+    a.display_by_year(2021);
 }
