5e76e50abbbf4d81194cf553eb2d406bc70b3b5a
Muhammad Hamza Kashif <68157276+hamzahisam@users.noreply.github.com>
2024-05-27 17:37:50 +0500

    Update functions.h

--- Vehicle Dealership Project/functions.h
+++ Vehicle Dealership Project/functions.h
@@ -1,6 +1,7 @@
 #pragma once
 bool isWithinRange(int number, int min, int max);
-void buy_vehicle(string type, Account* acc);
+void buy_vehicle(std::string type, Account* acc);
 void employeeMenu(Account* acc);
 void customerMenu(Account* acc);
 double update_Wallet();
+int isOk(int choice);
